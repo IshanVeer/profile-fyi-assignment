e7869af94774814dcff6d84bcf01889a9471a47d
Ishan Veer <ishanveer@Ishans-MacBook-Air.local>
2024-08-18 19:31:22 +0530

    fixed individual item type error

--- components/items/ItemDetail.tsx
+++ components/items/ItemDetail.tsx
@@ -14,25 +14,34 @@ interface Props {
   description: string;
 }
 interface ItemProps {
-  items: Props;
+  items: Props[];
 }
 
 const ItemDetail = ({ items }: ItemProps) => {
   const { id } = useParams();
+  const parsedId = Array.isArray(id) ? id[0] : id; // did because useParams either returns string or array of strings, but before it was a numebr and caused type error
   const [isItemAdded, setIsItemAdded] = useState(false);
   const cartCtx = useContext(CartContext);
   console.log(id, 'id');
   console.log(items, 'items');
-  const individualItem = items.find((item: Props) => item.id === parseInt(id));
+  const individualItem = items.find(
+    (item: Props) => item.id === parseInt(parsedId)
+  );
   const addToCartHandler = () => {
-    cartCtx.addItem(individualItem);
-    setIsItemAdded(true);
-    setTimeout(() => setIsItemAdded(false), 1000);
+    if (individualItem) {
+      cartCtx.addItem(individualItem);
+      setIsItemAdded(true);
+      setTimeout(() => setIsItemAdded(false), 1000);
+    }
   };
+
+  if (!individualItem) {
+    return <div>Item not found</div>;
+  }
 
   return (
     <div>
-      <div className='px-8 py-12 gap-20 flex '>
+      <div className='flex gap-20 px-8 py-12 '>
         <Image
           src={individualItem.image}
           alt={individualItem.title}
@@ -45,13 +54,13 @@ const ItemDetail = ({ items }: ItemProps) => {
             {individualItem.title.slice(0, 20)}
           </h2>
           <p className='h3-semibold'>{`$${individualItem.price}`}</p>
-          <p className='base-medium w-[60%] text-neutral-500'>
+          <p className='base-medium w-3/5 text-neutral-500'>
             {individualItem.description}
           </p>
           <Button
             onClick={addToCartHandler}
-            className={`my-4 w-[20%] bg-black text-neutral-100 hover:bg-neutral-800 transition duration-500 ease-in-out ${
-              isItemAdded ? 'bg-green-500 hover:bg-green-500 text-white' : ''
+            className={`my-4 w-1/5 bg-black text-neutral-100 transition duration-500 ease-in-out hover:bg-neutral-800 ${
+              isItemAdded ? 'bg-green-500 text-white hover:bg-green-500' : ''
             }`}
           >
             {isItemAdded ? 'Added To Cart' : 'Add To Cart'}
